fc1756f04f266e1263f1c853e9a9f58da2889dfc
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-12-05 08:56:43 +0600

    updated

--- src/main/java/driver/BaseDrive.java
+++ src/main/java/driver/BaseDrive.java
@@ -3,23 +3,41 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import utilities.PropertyDataParser;
 
+import java.time.Duration;
 import java.util.Properties;
 
 public class BaseDrive {
     //set webdriver type driver variable as null
     public WebDriver driver =null;
-   //Now extract drivers and site url
+
+    //Now extract drivers and site url
+    //create an object of properties class and initialize
     Properties props = PropertyDataParser.props("browserData");
+    // extract
+    String browser = props.getProperty("b1");
+    String siteurl = props.getProperty("baseUrl");
+
     //setup the driver
-    public WebDriver setup(String browser){
+    public WebDriver setup(){
         switch (browser){
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver=new ChromeDriver();
-                
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+            default:
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(siteurl);
+
         return driver;
     }
 }
--- src/main/java/driver/PageDriver.java
+++ src/main/java/driver/PageDriver.java
@@ -1,4 +1,9 @@
 package driver;
 
+import org.openqa.selenium.WebDriver;
+
 public class PageDriver {
+    //threadlocal 
+    private static final ThreadLocal<WebDriver> webdriver =new ThreadLocal<>();
+    private static PageDriver
 }
